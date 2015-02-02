9ab64dc51dafc8f1baf8efb2b317eb13028c4c56
karupanerura <karupa@cpan.org>
2015-02-02 18:52:01 +0900

    Checking in changes prior to tagging of version 0.01_02.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index e3730df..6d4aef7 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension Aniki
    
     {{$NEXT}}
    
    +0.01_02 2015-02-02T09:51:41Z
    +
    +    - Fixed guessing relationship name algorithm.
    +    - Fixed dependencies
    +
     0.01_01 2015-01-22T10:30:31Z
    
         - original version

--- lib/Aniki.pm
+++ lib/Aniki.pm
@@ -7,7 +7,7 @@ package Aniki {
     use Aniki::Schema;
     use Aniki::QueryBuilder;
 
-    our $VERSION = '0.01_01';
+    our $VERSION = '0.01_02';
 
     use Module::Load ();
     use SQL::Maker::SQLType qw/sql_type/;
