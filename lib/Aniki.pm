f6869b36aa35df9cd7465fee33db8f4f93434417
karupanerura <karupa@cpan.org>
2015-05-01 18:45:19 +0900

    Checking in changes prior to tagging of version 0.02_02.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 4e98cc4..f54a7b7 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension Aniki
    
     {{$NEXT}}
    
    +0.02_02 2015-05-01T09:44:45Z
    +
    +    - Added testing for `Aniki::Plugin::Pager`.
    +    - Something fixes.
    +
     0.02_01 2015-04-20T23:43:26Z
    
         - Use strict query builder mode by default.

--- lib/Aniki.pm
+++ lib/Aniki.pm
@@ -8,7 +8,7 @@ package Aniki {
     use Aniki::Schema;
     use Aniki::QueryBuilder;
 
-    our $VERSION = '0.02_01';
+    our $VERSION = '0.02_02';
 
     use SQL::Maker::SQLType qw/sql_type/;
     use DBIx::Sunny;
