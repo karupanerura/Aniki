0f1c4db5a66a96ffb3ddcbde443ecbcba771ecd9
karupanerura <karupa@cpan.org>
2015-05-22 20:07:01 +0900

    Checking in changes prior to tagging of version 0.02_03.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index f54a7b7..680f6a1 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension Aniki
    
     {{$NEXT}}
    
    +0.02_03 2015-05-22T11:06:35Z
    +
    +    - Added testing for `Aniki::Plugin::Count`.
    +    - Something fixes.
    +
     0.02_02 2015-05-01T09:44:45Z
    
         - Added testing for `Aniki::Plugin::Pager`.

--- lib/Aniki.pm
+++ lib/Aniki.pm
@@ -8,7 +8,7 @@ package Aniki {
     use Aniki::Schema;
     use Aniki::QueryBuilder;
 
-    our $VERSION = '0.02_02';
+    our $VERSION = '0.02_03';
 
     use SQL::Maker::SQLType qw/sql_type/;
     use DBIx::Sunny;
