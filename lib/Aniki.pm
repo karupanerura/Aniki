dac2cb7d01ce9c866fde29a3e9b383ab0a5c86f4
karupanerura <karupa@cpan.org>
2015-07-28 11:41:44 +0900

    Checking in changes prior to tagging of version 0.02_07.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index bf12d12..4a00cb6 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Aniki
    
     {{$NEXT}}
    
    +0.02_07 2015-07-28T02:40:43Z
    +
    +    - Change pluralize logic. (now pluralize last word only. SEE ALSO: https://github.com/karupanerura/Aniki/commit/13998800dc48fbed7ca1ca3bae64935c4aad78f5)
    +
     0.02_06 2015-07-27T04:45:03Z
    
         - Added API `new_row_from_hashref` and `new_collection_from_arrayref`.

--- lib/Aniki.pm
+++ lib/Aniki.pm
@@ -8,7 +8,7 @@ package Aniki {
     use Aniki::Schema;
     use Aniki::QueryBuilder;
 
-    our $VERSION = '0.02_06';
+    our $VERSION = '0.02_07';
 
     use SQL::Maker::SQLType qw/sql_type/;
     use DBIx::Sunny;
