6ff386877af1e9999a9b4e99d023c22aa969ed52
karupanerura <karupa@cpan.org>
2015-09-22 13:58:45 +0900

    Checking in changes prior to tagging of version 0.03_01.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 2eb6887..6039bf4 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Aniki
    
     {{$NEXT}}
    
    +0.03_01 2015-09-22T04:58:09Z
    +
    +    - Support automatic pre-cache the inverse relational rows
    +
     0.02_09 2015-09-19T05:23:54Z
    
         - Support the canonicalize of the `SELECT` SQL statement

--- lib/Aniki.pm
+++ lib/Aniki.pm
@@ -8,7 +8,7 @@ package Aniki {
     use Aniki::Schema;
     use Aniki::QueryBuilder;
 
-    our $VERSION = '0.02_09';
+    our $VERSION = '0.03_01';
 
     use SQL::Maker::SQLType qw/sql_type/;
     use DBIx::Sunny;
