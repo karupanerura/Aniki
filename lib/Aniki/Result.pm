7312f19a8737c706c9dab5dd9d2dd75db7589a22
karupanerura <karupa@cpan.org>
2015-10-03 17:00:49 +0900

    Inside-out Aniki instance from Aniki::Result instance (for Data::Dumper friendly)

--- lib/Aniki/Result.pm
+++ lib/Aniki/Result.pm
@@ -1,33 +1,40 @@
 package Aniki::Result {
     use namespace::sweep;
     use Mouse v2.4.5;
-    use overload
-        '@{}'    => sub { shift->rows },
-        fallback => 1;
+    use Hash::Util qw/fieldhash/;
 
     has table_name => (
         is       => 'ro',
         required => 1,
     );
 
-    has handler => (
-        is       => 'ro',
-        required => 1,
-        weak_ref => 1,
-    );
-
     has suppress_row_objects => (
         is      => 'rw',
+        lazy    => 1,
         default => sub { shift->handler->suppress_row_objects },
     );
 
     has row_class => (
         is      => 'rw',
+        lazy    => 1,
         default => sub {
             my $self = shift;
             $self->handler->guess_row_class($self->table_name);
         },
     );
+
+    fieldhash my %handler;
+
+    around new => sub {
+        my $orig = shift;
+        my ($class, %args) = @_;
+        my $handler = delete $args{handler};
+        my $self = $class->$orig(%args);
+        $handler{$self} = $handler;
+        return $self;
+    };
+
+    sub handler { $handler{+shift} }
 };
 
 1;
