920b0cf647b0124d7b97bda27d1ee5de7679f6e0
karupanerura <karupa@cpan.org>
2015-10-28 00:39:18 +0900

    Checking in changes prior to tagging of version 0.82.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index c540a3d..6d1d021 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Aniki
    
     {{$NEXT}}
    
    +0.82 2015-10-27T15:38:40Z
    +
    +   - Use Test::Requires for checking the modules installed (syohex)
    +
     0.81 2015-10-27T03:20:57Z
    
        - switch to old style package declation syntax for some old static code analyzers

--- lib/Aniki.pm
+++ lib/Aniki.pm
@@ -12,7 +12,7 @@ use Aniki::Schema;
 use Aniki::QueryBuilder;
 use Aniki::QueryBuilder::Canonical;
 
-our $VERSION = '0.81';
+our $VERSION = '0.82';
 
 use SQL::Maker::SQLType qw/sql_type/;
 use DBIx::Handler;
