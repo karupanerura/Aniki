f62f59973b8d9aeff1731e6f6260ca7fb9694ead
karupanerura <karupa@cpan.org>
2015-10-28 12:20:58 +0900

    Checking in changes prior to tagging of version 0.83.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 6d1d021..914414a 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Aniki
    
     {{$NEXT}}
    
    +0.83 2015-10-28T03:20:40Z
    +
    +   - Follow `0` and NULL in default value on insert_and_emulate_row method.
    +
     0.82 2015-10-27T15:38:40Z
    
        - Use Test::Requires for checking the modules installed (syohex)

--- lib/Aniki.pm
+++ lib/Aniki.pm
@@ -12,7 +12,7 @@ use Aniki::Schema;
 use Aniki::QueryBuilder;
 use Aniki::QueryBuilder::Canonical;
 
-our $VERSION = '0.82';
+our $VERSION = '0.83';
 
 use SQL::Maker::SQLType qw/sql_type/;
 use DBIx::Handler;
