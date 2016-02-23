1891a56cb0709af81f33d3983a700f71f77b7aff
karupanerura <karupa@cpan.org>
2016-02-23 15:40:24 +0900

    Checking in changes prior to tagging of version 0.91.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 60a25c1..9054361 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,8 @@ Revision history for Perl extension Aniki
    
     {{$NEXT}}
    
    +0.91 2016-02-23T06:24:26Z
    +
        - support `trace_query` option
    
     0.90 2016-02-23T02:11:26Z

--- lib/Aniki.pm
+++ lib/Aniki.pm
@@ -13,7 +13,7 @@ use Aniki::Schema;
 use Aniki::QueryBuilder;
 use Aniki::QueryBuilder::Canonical;
 
-our $VERSION = '0.90';
+our $VERSION = '0.91';
 
 use SQL::Maker::SQLType qw/sql_type/;
 use Class::Inspector;
