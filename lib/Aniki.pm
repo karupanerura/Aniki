8afb4817ecaa98f4d84dc61852b56298aa2d6b64
karupanerura <karupa@cpan.org>
2016-04-09 12:24:20 +0900

    Checking in changes prior to tagging of version 0.93.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 811a67e..48a64cb 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension Aniki
    
     {{$NEXT}}
    
    +0.93 2016-04-09T03:22:49Z
    +
    +   - set empty value if not found reletad rows for preformance
    +   - [EXPERIMENTAL] allows extra where condition with prefetch
    +
     0.92 2016-03-02T03:29:01Z
    
        - follow the null values in relationship

--- lib/Aniki.pm
+++ lib/Aniki.pm
@@ -13,7 +13,7 @@ use Aniki::Schema;
 use Aniki::QueryBuilder;
 use Aniki::QueryBuilder::Canonical;
 
-our $VERSION = '0.92';
+our $VERSION = '0.93';
 
 use SQL::Maker::SQLType qw/sql_type/;
 use Class::Inspector;
