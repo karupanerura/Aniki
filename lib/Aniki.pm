0f8a3029f0de8330b0530e1bdff4f4a85c6fde7c
karupanerura <karupa@cpan.org>
2017-06-21 16:01:27 +0900

    Checking in changes prior to tagging of version 1.04_02.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 6e57eaa..37f2658 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Aniki
    
     {{$NEXT}}
    
    +1.04_02 2017-06-21T07:00:32Z
    +
    +   - fixed packaging
    +
     1.04_01 2017-06-21T06:55:28Z
    
        - added update_and_(fetch|emulate)_row method (mamimumemoomoo)

--- lib/Aniki.pm
+++ lib/Aniki.pm
@@ -13,7 +13,7 @@ use Aniki::Schema;
 use Aniki::QueryBuilder;
 use Aniki::QueryBuilder::Canonical;
 
-our $VERSION = '1.04_01';
+our $VERSION = '1.04_02';
 
 use SQL::Maker::SQLType qw/sql_type/;
 use Class::Inspector;
